9c1168ad7a2cb0b62d688350b6f6620cc31f1d64
igormeister <igor.sherstuk8229@gmail.com>
2019-12-26 09:57:39 +0200

    server

--- functions/src/server.js
+++ functions/src/server.js
@@ -5,7 +5,6 @@ const app=express();
 
 app.use(cors({origin:true}))
     .use(bodyParser.json())
-    .use(bodyParser.urlencoded({extended:false}))
     .use("/math",require("./lib/math/route"))
     .get('*',(_,res)=>res.status(404)
         .json({success:false,data:"Endpoint noy found"}));
